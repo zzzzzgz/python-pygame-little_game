b9ba44da06bf335ff93f54f0e64678b556accb00
zzzzzgz <125903470+zzzzzgz@users.noreply.github.com>
2023-02-21 00:26:25 +0800

    Delete GAME.py

--- GAME.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class over():
-    def __init__(self):
-        self.image1 = pygame.image.load("image/gameover.png").convert_alpha()
-        self.image2 = pygame.image.load("image/gameover2.png").convert_alpha()
-        self.rect1 = self.image1.get_rect()
-        self.rect2 = self.image2.get_rect()
